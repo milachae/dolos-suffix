6907ab4c50e1889647413f2790fcb9913892c5f6
Michiel Lachaert <michiellachaert@outlook.com>
2025-01-29 14:04:10 +0100

    remove unnecessary examples

--- index.ts
+++ index.ts
@@ -4,34 +4,11 @@ import {generateRandomStrings, testAllSubstrings} from "./src/test/_util.js";
 
 
 function main(): void {
+    const strings = generateRandomStrings(1000, 1000, 26);
+
     console.time("")
-    const strings = generateRandomStrings(10000, 100, 10);
     const tree = new SuffixTree(strings);
     console.timeEnd("")
 }
-function test(): void {
-    const inputs = [ 'CAECEAB', 'BAECEAB' ];
-    const tree: SuffixTree = new SuffixTree(inputs);
-    tree.print();
-}
 
-function test2(): void {
-    // const inputs = ['CBAB', 'CBAA','BBBA', 'CCBA']
-    const inputs = ['QCBA', 'FCBA']
-    const tree: SuffixTree = new SuffixTree(inputs);
-    tree.print();
-}
-
-function test3(): void {
-    const inputs = ['AB', 'CB']
-    const tree: SuffixTree = new SuffixTree(inputs);
-    tree.print();
-}
-
-function test4(): void {
-    const inputs = [ 'ABCABD', 'AD' ];
-    const tree: SuffixTree = new SuffixTree(inputs);
-    tree.print();
-}
-
-test4()
+main()
