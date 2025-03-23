321a23ceda3f95bf9e60546efa3c3842c98ea8c9
Michiel Lachaert <michiellachaert@outlook.com>
2025-03-23 22:56:35 +0100

    cleanup tests maximal pairs

--- src/test/suffixTreeMaximalPairs.test.js.ts
+++ src/test/suffixTreeMaximalPairs.test.js.ts
@@ -2,9 +2,24 @@ import test, {ExecutionContext} from "ava";
 import {SuffixTree, MaximalPair} from "../lib/suffixTree.js";
 import {stringToNumbers} from "./_util.js"
 
+function maximalPairsStrings(inputs: string[]) {
+    const numberInputs = inputs.map(input => stringToNumbers(input))
+    const suffixTree = new SuffixTree(numberInputs)
+    return suffixTree.maximalPairs()
+}
+
+const testMaximalPairsActualExpected = test.macro((t, inputs: string[], expectedResult: MaximalPair[]) => {
+    const actual =maximalPairsStrings(inputs)
+    compareMaximalPairs(actual, expectedResult, t);
+});
+
+const testMaximalPairsLength = test.macro((t, inputs: string[], length: number) => {
+    const actual =maximalPairsStrings(inputs)
+    t.is(actual.length, 0);
+});
+
 function compareMaximalPairs(actual: MaximalPair[], expected: MaximalPair[], t: ExecutionContext) {
     t.is(actual.length, expected.length);
-
     for (const actualPair of actual) {
         let found = false;
         actualPair.starts.sort()
@@ -22,140 +37,74 @@ function compareMaximalPairs(actual: MaximalPair[], expected: MaximalPair[], t: 
     }
 }
 
-test("Should not find a pair in the same sequence", t => {
-    const input1 = stringToNumbers("xabcyabcz");
-    const suffixTree = new SuffixTree([input1]);
-    const actual = suffixTree.maximalPairs();
+test("Should not find a pair in the same sequence", testMaximalPairsLength, ["xabcyabcz"], 0);
 
-    t.is(actual.length, 0);
-})
+test("Should not find the end character as maximal pair",testMaximalPairsLength, ["abcd", "efgh"], 0);
 
-test("Should not find the end character as maximal pair", t => {
-    const input1 = stringToNumbers("abcd")
-    const input2 = stringToNumbers("efgh")
-    const suffixTree = new SuffixTree([input1, input2])
-    const actual = suffixTree.maximalPairs()
+test(
+    "Should not include the end character to the total length in the maximal pair",
+    testMaximalPairsActualExpected,
+    ["qab", "cab"],
+    [{starts: [{start: 1, input: 0}, {start: 1, input: 1}], length: 2}]
+)
 
-    t.is(actual.length, 0);
-})
+test(
+    "Should find maximal pairs between 2 inputs", testMaximalPairsActualExpected,
+    ["xabcy", "zabcq"],
+    [{starts: [{start: 1, input: 1}, {start: 1, input: 0}], length: 3}]
+)
 
-test("Should not include the end character to the total length in the maximal pair", t => {
-    const input1 = stringToNumbers("qab")
-    const input2 = stringToNumbers("cab")
-    const suffixTree = new SuffixTree([input1, input2])
-    const actual = suffixTree.maximalPairs()
+test(
+    "Should find maximal pairs at the end of a sequence",
+    testMaximalPairsActualExpected,
+    ["qabc", "zabc"],
+    [{starts: [{start: 1, input: 0}, {start: 1, input: 1}], length: 3}]
+)
 
-    const expected: MaximalPair[] =[
-        {starts: [{start: 1, input: 0}, {start: 1, input: 1}], length: 2}
-    ]
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected)
-})
+test(
+    "Should find maximal pairs at the begin of a sequence",
+    testMaximalPairsActualExpected,
+    ["abcq", "abcz"],
+    [{starts: [{start: 0, input: 1}, {start: 0, input: 0}], length: 3}]
+)
 
-test("Should find maximal pairs between 2 inputs", t => {
-    const input1 = stringToNumbers("xabcy");
-    const input2 = stringToNumbers("zabcq");
-
-    const suffixTree = new SuffixTree([input1, input2]);
-    const actual = suffixTree.maximalPairs();
-
-    const expected: MaximalPair[] =[
-        {starts: [{start: 1, input: 1}, {start: 1, input: 0}], length: 3}
-    ];
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected);
-})
-
-test("Should find maximal pairs at the end of a sequence", t => {
-    const input1 = stringToNumbers("qabc");
-    const input2 = stringToNumbers("zabc");
-
-    const suffixTree = new SuffixTree([input1, input2]);
-    const actual = suffixTree.maximalPairs();
-
-    const expected: MaximalPair[] =[
-        {starts: [{start: 1, input: 0}, {start: 1, input: 1}], length: 3}
-    ];
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected);
-})
-
-test("Should find maximal pairs at the begin of a sequence", t => {
-    const input1 = stringToNumbers("abcq");
-    const input2 = stringToNumbers("abcz");
-
-    const suffixTree = new SuffixTree([input1, input2]);
-    const actual = suffixTree.maximalPairs();
-
-    const expected: MaximalPair[] =[
-        {starts: [{start: 0, input: 1}, {start: 0, input: 0}], length: 3}
-    ];
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected);
-})
-
-test("Should find the same pair between inputs", t => {
-
-    const input1 = stringToNumbers("qabcz");
-    const input2 = stringToNumbers("yabcx");
-    const input3 = stringToNumbers("kabcj");
-
-    const suffixTree = new SuffixTree([input1, input2, input3]);
-    const actual = suffixTree.maximalPairs();
-
-    const expected: MaximalPair[] = [
+test(
+    "Should find the same pair between inputs",
+    testMaximalPairsActualExpected,
+    ["qabcz", "yabcx", "kabcj"],
+    [
         {starts: [{start: 1, input: 1}, {start: 1, input: 0}], length: 3},
         {starts: [{start: 1, input: 1}, {start: 1, input: 2}], length: 3},
         {starts: [{start: 1, input: 0}, {start: 1, input: 2}], length: 3}
     ]
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected)
+)
 
-})
-
-test("Should find multiple pairs of the same sequence", t => {
-    const input1 = stringToNumbers("jABCk")
-    const input2 = stringToNumbers("xABCyABCz")
-
-    const suffixTree = new SuffixTree([input1, input2]);
-    const actual = suffixTree.maximalPairs();
-
-    const expected: MaximalPair[] = [
+test(
+    "Should find multiple pairs of the same sequence",
+    testMaximalPairsActualExpected,
+    ["jABCk", "xABCyABCz"],
+    [
         {starts: [{start: 1, input: 1}, {start: 1, input: 0}], length: 3},
         {starts: [{start: 1, input: 0}, {start: 5, input: 1}], length: 3}
     ]
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected)
-})
+)
 
-test("Should find for every pair of sequence the maximum pairs", t => {
-    const input1 = stringToNumbers("xabcyi")
-    const input2 = stringToNumbers("zabck")
-    const input3 = stringToNumbers("qabcyj")
-
-    const suffixTree = new SuffixTree([input1, input2, input3])
-    const actual = suffixTree.maximalPairs()
-    const expected: MaximalPair[] =[
+test(
+    "Should find for every pair of sequence the maximum pairs",
+    testMaximalPairsActualExpected,
+    ["xabcyi", "zabck", "qabcyj"],
+    [
         {starts: [{start: 1, input: 2}, {start: 1, input:0}], length: 4},
         {starts: [{start: 1, input: 1}, {start: 1, input: 2}], length: 3},
         {starts: [{start: 1, input: 1}, {start: 1, input: 0}], length: 3}
     ]
-    compareMaximalPairs(actual, expected, t);
-    t.deepEqual(actual, expected)
-})
+)
 
-test("Should contain the start position of the correct input when in a leaf", t => {
-    const input1 = stringToNumbers("xABC")
-    const input2 = stringToNumbers("qqABC")
-
-    const suffixTree = new SuffixTree([input1, input2])
-    const actual = suffixTree.maximalPairs()
-
-    const expected: MaximalPair[] =[
+test(
+    "Should contain the start position of the correct input when in a leaf",
+    testMaximalPairsActualExpected,
+    ["xABC", "qqABC"],
+    [
         {starts: [{start: 1, input:0}, {start: 2, input: 1}], length: 3},
     ]
-
-    t.deepEqual(actual, expected);
-    compareMaximalPairs(actual, expected, t);
-
-})
+)
